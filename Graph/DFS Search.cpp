17cbb6e93897bb192a500b264d4bc879c8e11f5f
besthuman012 <75174918+besthuman012@users.noreply.github.com>
2021-05-14 00:47:22 +0530

    Stak

---
+++ Graph/DFS Search.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+// BFS search using Queue
+
+void DFSearch(vector<int> adj[], int start, int vertices){
+
+    bool visited[vertices]; // Set all false initially
+    stack<int> stak;
+
+    for(int i = 0; i < vertices ; i++)  visited[i] = false;
+
+    stak.push(start);
+    visited[start] = true;
+    int custody;
+    cout << "DFS Traversal starting from " << start << " ....\n";
+    while(!stak.empty()){
+        custody = stak.top();
+        stak.pop();
+
+        cout << custody << " ";
+
+
+        for(int i = 0 ; i<adj[custody].size() ; i++){
+            if(visited[adj[custody][i]] == false){
+                stak.push(adj[custody][i]);
+                visited[adj[custody][i]] = true;
+            }
+        }
+    }
+}
+
+// Implementation of graph using Adjacency List
+void addEdge(vector<int> adj[],int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void printGraph(vector<int> adj[],int V){
+    for(int i = 0; i < V ; i++){
+        cout << "Adjancency List " << i << " ...\n Head ";
+        for(int j = 0; j < adj[i].size(); j++){
+            cout<< " -->> " << adj[i][j];
+        }
+        cout << "\n\n";
+    }
+}
+int main(){
+    int vertices = 5;
+    vector<int> adj[5];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    DFSearch(adj,0,vertices);
+    //printGraph(adj,vertices);
+    return 0;
+}
