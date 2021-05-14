60cf2a9d43b96f13f24fa127bac19549dfeec37e
besthuman012 <75174918+besthuman012@users.noreply.github.com>
2021-05-14 10:49:13 +0530

    Parent + Visited Technique

---
+++ Graph/Detect Cycle in Undirected Graph.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+// Detect cycle in Directed Graph
+// Using One parent criteria
+// One child One Parent
+void DetectCycleUnDirected(vector<int> adj[], int start, int vertices){
+
+    bool visited[vertices]; // Set all false initially
+    int parent[vertices];
+    stack<int> stak;
+
+    for(int i = 0; i < vertices ; i++)  visited[i] = false;
+
+    stak.push(start);
+    visited[start] = true;
+    int custody;
+
+    while(!stak.empty()){
+        custody = stak.top();
+        stak.pop();
+
+
+        for(int i = 0 ; i<adj[custody].size() ; i++){
+            if(visited[adj[custody][i]] == false){
+                stak.push(adj[custody][i]);
+                visited[adj[custody][i]] = true;
+                parent[adj[custody][i]] = custody;
+            }
+            if(visited[adj[custody][i]] == true && parent[adj[custody][i]] != custody){
+                cout << "Cycle Detected .... \n";
+                return;
+            }
+        }
+    }
+    cout << " No Cycle .............. ";
+}
+
+// Implementation of graph using Adjacency List
+void addEdge(vector<int> adj[],int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void printGraph(vector<int> adj[],int V){
+    for(int i = 0; i < V ; i++){
+        cout << "Adjancency List " << i << " ...\n Head ";
+        for(int j = 0; j < adj[i].size(); j++){
+            cout<< " -->> " << adj[i][j];
+        }
+        cout << "\n\n";
+    }
+}
+int main(){
+    int vertices = 5;
+    vector<int> adj[5];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    DetectCycleUnDirected(adj,0,vertices);
+    //printGraph(adj,vertices);
+    return 0;
+}
